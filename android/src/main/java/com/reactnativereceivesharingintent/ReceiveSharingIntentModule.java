d4e59a2ddfd8e27c49f66cfb44d3276e0764c285
Michael Hemingway <mpghemingway@gmail.com>
2023-07-29 16:44:52 -0400

    fix: integrate fix for #110
    https://github.com/ajith-ab/react-native-receive-sharing-intent/issues/110#issuecomment-971612714

--- android/src/main/java/com/reactnativereceivesharingintent/ReceiveSharingIntentModule.java
+++ android/src/main/java/com/reactnativereceivesharingintent/ReceiveSharingIntentModule.java
@@ -3,8 +3,6 @@ package com.reactnativereceivesharingintent;
 import android.app.Activity;
 import android.app.Application;
 import android.content.Intent;
-
-
 import android.os.Build;
 import androidx.annotation.RequiresApi;
 import com.facebook.react.bridge.Promise;
@@ -12,45 +10,54 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 
-
 public class ReceiveSharingIntentModule extends ReactContextBaseJavaModule {
   public final String Log_Tag = "ReceiveSharingIntent";
 
   private final ReactApplicationContext reactContext;
   private ReceiveSharingIntentHelper receiveSharingIntentHelper;
+  private Intent oldIntent;
 
   public ReceiveSharingIntentModule(ReactApplicationContext reactContext) {
     super(reactContext);
     this.reactContext = reactContext;
-    Application applicationContext = (Application) reactContext.getApplicationContext();
-    receiveSharingIntentHelper = new ReceiveSharingIntentHelper(applicationContext);
+    Application applicationContext =
+        (Application)reactContext.getApplicationContext();
+    receiveSharingIntentHelper =
+        new ReceiveSharingIntentHelper(applicationContext);
   }
-
 
   protected void onNewIntent(Intent intent) {
     Activity mActivity = getCurrentActivity();
-    if(mActivity == null) { return; }
+    if (mActivity == null) {
+      return;
+    }
+    oldIntent = mActivity.getIntent();
     mActivity.setIntent(intent);
   }
 
   @RequiresApi(api = Build.VERSION_CODES.KITKAT)
   @ReactMethod
-  public void getFileNames(Promise promise){
+  public void getFileNames(Promise promise) {
     Activity mActivity = getCurrentActivity();
-    if(mActivity == null) { return; }
+    if (mActivity == null) {
+      return;
+    }
     Intent intent = mActivity.getIntent();
     receiveSharingIntentHelper.sendFileNames(reactContext, intent, promise);
-    mActivity.setIntent(null);
+    if (oldIntent != null) {
+      mActivity.setIntent(oldIntent);
+    }
   }
 
   @ReactMethod
-  public void clearFileNames(){
+  public void clearFileNames() {
     Activity mActivity = getCurrentActivity();
-    if(mActivity == null) { return; }
+    if (mActivity == null) {
+      return;
+    }
     Intent intent = mActivity.getIntent();
     receiveSharingIntentHelper.clearFileNames(intent);
   }
-
 
   @Override
   public String getName() {
