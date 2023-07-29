dfb9b4a2aaeddc560a051997fd01d10dc44bea27
Michael Hemingway <mpghemingway@gmail.com>
2023-07-29 17:10:34 -0400

    fix: integrate changes from null guard discussions, pr

--- android/src/main/java/com/reactnativereceivesharingintent/ReceiveSharingIntentHelper.java
+++ android/src/main/java/com/reactnativereceivesharingintent/ReceiveSharingIntentHelper.java
@@ -8,12 +8,10 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
 import android.provider.OpenableColumns;
-
 import androidx.annotation.RequiresApi;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.bridge.WritableNativeMap;
-
 import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.Objects;
@@ -22,164 +20,186 @@ public class ReceiveSharingIntentHelper {
 
   private Context context;
 
-  public ReceiveSharingIntentHelper(Application context){
+  public ReceiveSharingIntentHelper(Application context) {
     this.context = context;
   }
 
   @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-  public void sendFileNames(Context context, Intent intent, Promise promise){
+  public void sendFileNames(Context context, Intent intent, Promise promise) {
     try {
       String action = intent.getAction();
       String type = intent.getType();
-      if(type == null) { return; }
-      if(!type.startsWith("text") && (Objects.equals(action, Intent.ACTION_SEND) || Objects.equals(action, Intent.ACTION_SEND_MULTIPLE))){
-        WritableMap files = getMediaUris(intent,context);
-        if(files == null) return;
+      if (type == null) {
+        return;
+      }
+      if (!type.startsWith("text") &&
+          (Objects.equals(action, Intent.ACTION_SEND) ||
+           Objects.equals(action, Intent.ACTION_SEND_MULTIPLE))) {
+        WritableMap files = getMediaUris(intent, context);
         promise.resolve(files);
-      }else if(type.startsWith("text") && Objects.equals(action, Intent.ACTION_SEND)){
+      } else if (type.startsWith("text") &&
+                 Objects.equals(action, Intent.ACTION_SEND)) {
         String text = null;
         String subject = null;
-        try{
+        try {
           text = intent.getStringExtra(Intent.EXTRA_TEXT);
           subject = intent.getStringExtra(Intent.EXTRA_SUBJECT);
-        }catch (Exception ignored){ }
-        if(text == null){
-          WritableMap files = getMediaUris(intent,context);
-          if(files == null) return;
+        } catch (Exception ignored) {
+        }
+        if (text == null) {
+          WritableMap files = getMediaUris(intent, context);
           promise.resolve(files);
-        }else{
+        } else {
           WritableMap files = new WritableNativeMap();
           WritableMap file = new WritableNativeMap();
-          file.putString("contentUri",null);
-          file.putString("filePath", null);
-          file.putString("fileName", null);
-          file.putString("extension", null);
-          if(text.startsWith("http")){
+          if (text.startsWith("http")) {
             file.putString("weblink", text);
-            file.putString("text",null);
-          }else{
-            file.putString("weblink", null);
-            file.putString("text",text);
+          } else {
+            file.putString("text", text);
           }
           file.putString("subject", subject);
-          files.putMap("0",file);
+          files.putMap("0", file);
           promise.resolve(files);
         }
 
-      }else if(Objects.equals(action, Intent.ACTION_VIEW)){
+      } else if (Objects.equals(action, Intent.ACTION_VIEW)) {
         String link = intent.getDataString();
         WritableMap files = new WritableNativeMap();
         WritableMap file = new WritableNativeMap();
-        file.putString("contentUri",null);
-        file.putString("filePath", null);
-        file.putString("mimeType",null);
-        file.putString("text",null);
-        file.putString("weblink", link);
-        file.putString("fileName", null);
-        file.putString("extension", null);
-        files.putMap("0",file);
-        promise.resolve(files);
-      }
-      else if (Objects.equals(action, "android.intent.action.PROCESS_TEXT")) {
+        if (type.startsWith("image") || type.startsWith("video") ||
+            type.startsWith("audio")) {
+          String filePath = ReceiveSharingIntentGetFileDirectory.getFilePath(
+              context, Uri.parse(link));
+          file.putString("filePath", filePath);
+        } else {
+          file.putString("weblink", link);
+        }
+      } else if (Objects.equals(action, "android.intent.action.PROCESS_TEXT")) {
         String text = null;
         try {
           text = intent.getStringExtra(intent.EXTRA_PROCESS_TEXT);
         } catch (Exception e) {
         }
-          WritableMap files = new WritableNativeMap();
-          WritableMap file = new WritableNativeMap();
-          file.putString("contentUri", null);
-          file.putString("filePath", null);
-          file.putString("fileName", null);
-          file.putString("extension", null);
-          file.putString("weblink", null);
-          file.putString("text", text);
-          files.putMap("0", file);
-          promise.resolve(files);
-      }else{
-        promise.reject("error","Invalid file type.");
+        WritableMap files = new WritableNativeMap();
+        WritableMap file = new WritableNativeMap();
+        file.putString("contentUri", null);
+        file.putString("filePath", null);
+        file.putString("fileName", null);
+        file.putString("extension", null);
+        file.putString("weblink", null);
+        file.putString("text", text);
+      } else if (Objects.equals(action, "android.intent.action.PROCESS_TEXT")) {
+        String text = null;
+        try {
+          text = intent.getStringExtra(intent.EXTRA_PROCESS_TEXT);
+        } catch (Exception e) {
+        }
+        WritableMap files = new WritableNativeMap();
+        WritableMap file = new WritableNativeMap();
+        file.putString("contentUri", null);
+        file.putString("filePath", null);
+        file.putString("fileName", null);
+        file.putString("extension", null);
+        file.putString("weblink", null);
+        file.putString("text", text);
+        files.putMap("0", file);
+        promise.resolve(files);
+      } else {
+        promise.reject("error", "Invalid file type.");
       }
-    }catch (Exception e){
-      promise.reject("error",e.toString());
+    } catch (Exception e) {
+      promise.reject("error", e.toString());
     }
   };
 
-
   @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-  public WritableMap getMediaUris(Intent intent, Context context){
-    if (intent == null) return null;
-
+  public WritableMap getMediaUris(Intent intent, Context context) {
+    if (intent == null)
+      return null;
     String subject = null;
-    try{
-      subject = intent.getStringExtra(Intent.EXTRA_SUBJECT);
-    }catch (Exception ignored){ }
-
     WritableMap files = new WritableNativeMap();
-    if(Objects.equals(intent.getAction(), Intent.ACTION_SEND)){
+    if (Objects.equals(intent.getAction(), Intent.ACTION_SEND)) {
       WritableMap file = new WritableNativeMap();
-      Uri contentUri = (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM);
-      if(contentUri == null) return null;
-      String filePath = FileDirectory.INSTANCE.getAbsolutePath(context, contentUri);
-      ContentResolver contentResolver = context.getContentResolver();
-      file.putString("mimeType", contentResolver.getType(contentUri));
-      Cursor queryResult = contentResolver.query(contentUri, null, null, null, null);
-      queryResult.moveToFirst();
-      file.putString("fileName", queryResult.getString(queryResult.getColumnIndex(OpenableColumns.DISPLAY_NAME)));
-      file.putString("filePath", filePath);
-      file.putString("contentUri",contentUri.toString());
-      file.putString("text",null);
-      file.putString("weblink", null);
-      file.putString("subject", subject);
-      files.putMap("0",file);
-    }else if(Objects.equals(intent.getAction(), Intent.ACTION_SEND_MULTIPLE)) {
-      ArrayList<Uri> contentUris = intent.getParcelableArrayListExtra(Intent.EXTRA_STREAM);
+      Uri contentUri = (Uri)intent.getParcelableExtra(Intent.EXTRA_STREAM);
+      if (contentUri != null) {
+        String filePath =
+            FileDirectory.INSTANCE.getAbsolutePath(context, contentUri);
+        ContentResolver contentResolver = context.getContentResolver();
+        Cursor queryResult =
+            contentResolver.query(contentUri, null, null, null, null);
+        queryResult.moveToFirst();
+        if (filePath != null) {
+          file.putString("fileName", getFileName(filePath));
+          file.putString("extension", getExtension(filePath));
+          file.putString("mimeType", contentResolver.getType(contentUri));
+          file.putString("filePath", filePath);
+          file.putString("subject", subject);
+        }
+        file.putString("contentUri", contentUri.toString());
+        files.putMap("0", file);
+      }
+    } else if (Objects.equals(intent.getAction(),
+                              Intent.ACTION_SEND_MULTIPLE)) {
+      ArrayList<Uri> contentUris =
+          intent.getParcelableArrayListExtra(Intent.EXTRA_STREAM);
       if (contentUris != null) {
         int index = 0;
         for (Uri uri : contentUris) {
           WritableMap file = new WritableNativeMap();
           ContentResolver contentResolver = context.getContentResolver();
-          String filePath = FileDirectory.INSTANCE.getAbsolutePath(context, uri);
-          // Based on https://developer.android.com/training/secure-file-sharing/retrieve-info
+          String filePath =
+              FileDirectory.INSTANCE.getAbsolutePath(context, uri);
+          // Based on
+          // https://developer.android.com/training/secure-file-sharing/retrieve-info
           file.putString("mimeType", contentResolver.getType(uri));
-          Cursor queryResult = contentResolver.query(uri, null, null, null, null);
+          Cursor queryResult =
+              contentResolver.query(uri, null, null, null, null);
           queryResult.moveToFirst();
-          file.putString("fileName", queryResult.getString(queryResult.getColumnIndex(OpenableColumns.DISPLAY_NAME)));
+          if (filePath != null) {
+            file.putString("fileName",
+                           queryResult.getString(queryResult.getColumnIndex(
+                               OpenableColumns.DISPLAY_NAME)));
+            file.putString("extension", getExtension(filePath));
+          } else {
+            file.putString("fileName", null);
+            file.putString("extension", null);
+          }
+          file.putString("contentUri", uri.toString());
           file.putString("filePath", filePath);
-          file.putString("contentUri",uri.toString());
-          file.putString("text",null);
+          file.putString("text", null);
           file.putString("weblink", null);
           file.putString("subject", subject);
-          files.putMap(Integer.toString(index),file);
+          files.putMap(Integer.toString(index), file);
           index++;
         }
       }
     }
-    return  files;
+    return files;
   }
 
-
-  private String getMediaType(String url){
+  private String getMediaType(String url) {
     String mimeType = URLConnection.guessContentTypeFromName(url);
     return mimeType;
   }
 
-
-  public void clearFileNames(Intent intent){
+  public void clearFileNames(Intent intent) {
     String type = intent.getType();
-    if(type == null) return;
+    if (type == null)
+      return;
     if (type.startsWith("text")) {
       intent.removeExtra(Intent.EXTRA_TEXT);
-    } else if (type.startsWith("image") || type.startsWith("video") || type.startsWith("application")) {
+    } else if (type.startsWith("image") || type.startsWith("video") ||
+               type.startsWith("application")) {
       intent.removeExtra(Intent.EXTRA_STREAM);
     }
+    intent.setData(null);
   }
 
-  public String getFileName(String file){
-    return  file.substring(file.lastIndexOf('/') + 1);
+  public String getFileName(String file) {
+    return file.substring(file.lastIndexOf('/') + 1);
   }
 
-  public String getExtension(String file){
+  public String getExtension(String file) {
     return file.substring(file.lastIndexOf('.') + 1);
   }
-
 }
